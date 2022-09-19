63c7b1553979b32f5db53b4e2aa519625bf73fee
Spencer Taira <spencermtaira@gmail.com>
2022-09-18 23:43:28 -0700

    added functionality to clear input fields, create image/div elements and added meme text

--- script.js
+++ script.js
@@ -2,6 +2,22 @@ document.addEventListener('DOMContentLoaded', function() {
   const form = document.querySelector('form');
   form.addEventListener('submit', (e) => {
     e.preventDefault();
-    console.log('test');
+    const imageLink = document.querySelector('#imageLink')
+    const memeText = document.querySelector('#memeText');
+    const img = document.createElement('img');
+    const testDiv = document.createElement('div');
+    const textDiv = document.createElement('p');
+    testDiv.style.position = 'relative';
+    textDiv.style.position = 'absolute';
+    testDiv.style.padding = '10px';
+    textDiv.innerHTML = 'testing';
+    textDiv.style.top = '0px';
+    form.append(testDiv);
+    textDiv.textContent = memeText.value;
+    testDiv.appendChild(textDiv);
+    img.src = imageLink.value;
+    imageLink.value = '';
+    memeText.value = '';
+    testDiv.append(img);
   });
 });
